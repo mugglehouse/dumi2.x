aabc0fe75db26386828d1de640952dabf005a484
xiaofangmo <xiaofangmo@tencent.com>
2025-06-19 18:18:50 +0800

    feat: 引入包测试

--- src/demo/demo1.tsx
+++ src/demo/demo1.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { Button} from 'antd';
 
-export const Demo1 = () => <h4>hello muggle</h4>;
+export const Demo1 = () => (
+    <>
+        <Button type="primary">Primary Button</Button>
+        <h4>hello muggle</h4>
+    </>
+);
 export default Demo1;
